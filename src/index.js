ad7b9b5e2e18268a156921a738b6a65faa09c382
Junjie Chen <junjie.chen18@gmail.com>
2020-06-07 19:49:22 -0400

    feat(component): update state of errorMessage

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ class App extends Component {
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ latitude: position.coords.latitude }),
-      error => error
+      error => this.setState({ errorMessage: error.message })
     );
   }
 
