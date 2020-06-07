17ffa4d92bb3bdbf6cc04ee40c1c621e917d67b4
Junjie Chen <junjie.chen18@gmail.com>
2020-06-07 16:41:44 -0400

    feat(component): create getSummerOrWinter helper method

--- src/SummerOrWinter.js
+++ src/SummerOrWinter.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+const getSummerOrWinter = (latitude, month) => {
+  if (month > 2 && month < 9) {
+    return latitude > 0 ? 'summer' : 'winter';
+  } else {
+    return latitude > 0 ? 'winter' : 'summer';
+  }
+};
+
 const SummerOrWinter = () => {
   return <div>Summer Or Winter</div>;
 };
