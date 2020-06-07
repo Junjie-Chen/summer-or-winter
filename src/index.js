1fe236452e52f01b46b9368839553acde3daad3a
Junjie Chen <junjie.chen18@gmail.com>
2020-06-07 19:52:27 -0400

    feat(component): render summer or winter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SummerOrWinter from './SummerOrWinter';
 
 class App extends Component {
   state = {
@@ -12,6 +13,12 @@ class App extends Component {
       position => this.setState({ latitude: position.coords.latitude }),
       error => this.setState({ errorMessage: error.message })
     );
+  }
+
+  renderContent() {
+    if (this.state.latitude && !this.state.errorMessage) {
+      return <SummerOrWinter />;
+    }
   }
 
   render() {
