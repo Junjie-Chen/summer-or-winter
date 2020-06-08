023dc3cc25be5fd797983d4423c522d17f96cd8d
Junjie Chen <junjie.chen18@gmail.com>
2020-06-07 22:16:44 -0400

    feat(hook): call getCurrentPosition method of geolocation API after calling useLocation hook

--- src/useLocation.js
+++ src/useLocation.js
@@ -1,8 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const useLocation = () => {
   const [latitude] = useState(null);
   const [errorMessage] = useState('');
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => position,
+      error => error
+    );
+  }, []);
 };
 
 export default useLocation;
